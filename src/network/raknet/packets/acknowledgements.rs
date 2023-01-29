cefdde9d4e83099a310459e33862f255a0e10b09
Ruben Adema <44266876+RadiatedMonkey@users.noreply.github.com>
2023-01-29 21:24:37 +0100

    acknowledgements.rs: Cleaned up duplicated fragments

--- src/network/raknet/packets/acknowledgements.rs
+++ src/network/raknet/packets/acknowledgements.rs
@@ -15,6 +15,45 @@ pub enum AcknowledgementRecord {
     Range(Range<u32>),
 }
 
+fn encode_records(mut buffer: BytesMut, records: &[AcknowledgementRecord])
+    -> anyhow::Result<BytesMut>
+{
+    buffer.put_i16(records.len() as i16);
+    for record in records {
+        match record {
+            AcknowledgementRecord::Single(id) => {
+                buffer.put_u8(1); // Is single
+                buffer.put_u24_le(*id);
+            }
+            AcknowledgementRecord::Range(range) => {
+                buffer.put_u8(0); // Is range
+                buffer.put_u24_le(range.start);
+                buffer.put_u24_le(range.end);
+            }
+        }
+    }
+
+    Ok(buffer)
+}
+
+fn decode_records(mut buffer: BytesMut) -> Vec<AcknowledgementRecord> {
+    let record_count = buffer.get_u16();
+    let mut records = Vec::with_capacity(record_count as usize);
+
+    for _ in 0..record_count {
+        let is_range = buffer.get_u8() == 0;
+        if is_range {
+            records.push(AcknowledgementRecord::Range(
+                buffer.get_u24_le()..buffer.get_u24_le(),
+            ));
+        } else {
+            records.push(AcknowledgementRecord::Single(buffer.get_u24_le()));
+        }
+    }
+
+    records
+}
+
 /// Confirms that packets have been received.
 #[derive(Debug)]
 pub struct Acknowledgement {
@@ -30,45 +69,15 @@ impl Acknowledgement {
 impl Encodable for Acknowledgement {
     fn encode(&self) -> anyhow::Result<BytesMut> {
         let mut buffer = BytesMut::with_capacity(10);
-
         buffer.put_u8(Self::ID);
-        buffer.put_i16(self.records.len() as i16);
-        for record in &self.records {
-            match record {
-                AcknowledgementRecord::Single(id) => {
-                    buffer.put_u8(1); // Is single
-                    buffer.put_u24_le(*id);
-                }
-                AcknowledgementRecord::Range(range) => {
-                    buffer.put_u8(0); // Is range
-                    buffer.put_u24_le(range.start);
-                    buffer.put_u24_le(range.end);
-                }
-            }
-        }
-
-        Ok(buffer)
+        encode_records(buffer, &self.records)
     }
 }
 
 impl Decodable for Acknowledgement {
     fn decode(mut buffer: BytesMut) -> anyhow::Result<Self> {
         vex_assert!(buffer.get_u8() == Self::ID);
-
-        let record_count = buffer.get_u16();
-        let mut records = Vec::with_capacity(record_count as usize);
-
-        for _ in 0..record_count {
-            let is_range = buffer.get_u8() == 0;
-            if is_range {
-                records.push(AcknowledgementRecord::Range(
-                    buffer.get_u24_le()..buffer.get_u24_le(),
-                ));
-            } else {
-                records.push(AcknowledgementRecord::Single(buffer.get_u24_le()));
-            }
-        }
-
+        let records = decode_records(buffer);
         Ok(Self { records })
     }
 }
@@ -88,45 +97,15 @@ impl NegativeAcknowledgement {
 impl Encodable for NegativeAcknowledgement {
     fn encode(&self) -> anyhow::Result<BytesMut> {
         let mut buffer = BytesMut::with_capacity(10);
-
         buffer.put_u8(Self::ID);
-        buffer.put_i16(self.records.len() as i16);
-        for record in &self.records {
-            match record {
-                AcknowledgementRecord::Single(id) => {
-                    buffer.put_u8(1); // Is single
-                    buffer.put_u24_le(*id);
-                }
-                AcknowledgementRecord::Range(range) => {
-                    buffer.put_u8(0); // Is range
-                    buffer.put_u24_le(range.start);
-                    buffer.put_u24_le(range.end);
-                }
-            }
-        }
-
-        Ok(buffer)
+        encode_records(buffer, &self.records)
     }
 }
 
 impl Decodable for NegativeAcknowledgement {
     fn decode(mut buffer: BytesMut) -> anyhow::Result<Self> {
         vex_assert!(buffer.get_u8() == Self::ID);
-
-        let record_count = buffer.get_u16();
-        let mut records = Vec::with_capacity(record_count as usize);
-
-        for _ in 0..record_count {
-            let is_range = buffer.get_u8() == 0;
-            if is_range {
-                records.push(AcknowledgementRecord::Single(buffer.get_u24_le()));
-            } else {
-                records.push(AcknowledgementRecord::Range(
-                    buffer.get_u24_le()..buffer.get_u24_le(),
-                ));
-            }
-        }
-
+        let records = decode_records(buffer);
         Ok(Self { records })
     }
 }
