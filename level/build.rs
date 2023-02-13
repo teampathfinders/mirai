35ec801aac8075cfb4b8532c194fe5bbda969e15
Ruben Adema <44266876+RadiatedMonkey@users.noreply.github.com>
2023-02-13 17:09:52 +0100

    Added LevelDB wrapper from my own repo

---
+++ level/build.rs
@@ -0,0 +1,20 @@
+use std::process::Command;
+
+fn main() {
+    let dst = cmake::build("leveldb");
+
+    #[cfg(debug_assertions)]
+    {
+        println!("cargo:rustc-link-search=native={}/build/Debug", dst.display());
+        println!("cargo:rustc-link-search=native={}/build/vendor/Debug", dst.display());
+    }
+
+    #[cfg(not(debug_assertions))]
+    {
+        println!("cargo:rustc-link-search=native={}/build/Release", dst.display());
+        println!("cargo:rustc-link-search=native={}/build/vendor/Release", dst.display());
+    }
+
+    println!("cargo:rustc-link-lib=static=leveldb-wrapper");
+    println!("cargo:rustc-link-lib=static=leveldb-mcpe");
+}
---
+++ level/leveldb/leveldb.h
@@ -0,0 +1,25 @@
+#ifdef __cplusplus
+extern "C" {
+#endif __cplusplus
+
+    // Result returned by fallible operations.
+    // Data must manually be freed after use.
+    struct LevelResult {
+        int is_success;
+        int size;
+        void* data;
+    };
+
+    // Open a LevelDB database.
+    struct LevelResult level_open_database(const char* path);
+    // Close a LevelDB database.
+    // This also frees the pointers, it must no longer be used.
+    void level_close_database(void* database);
+    // Loads a key from the database.
+    struct LevelResult level_get_key(void* database, const char* key);
+    // Deallocates a string previously allocated by another function.
+    void level_deallocate_array(char* array);
+
+#ifdef __cplusplus
+}
+#endif __cplusplus
