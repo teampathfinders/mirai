359196b65ecf39f8f6582d00de81fa64bfa80c6a
Ruben Adema <44266876+RadiatedMonkey@users.noreply.github.com>
2023-04-02 17:29:26 +0200

    Add CompilationCache implementation

--- core/src/extension/cache.rs
+++ core/src/extension/cache.rs
@@ -1,25 +1,62 @@
+use std::fs::File;
+use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
+use anyhow::{anyhow, Context};
+use sha2::{Sha256, Digest};
 use wasmtime::{Engine, Module};
 
+use super::ASSEMBLY_DIRECTORY;
+
 pub struct CompilationCache {
-    dir: PathBuf
+    cache_dir: PathBuf
 }
 
 impl CompilationCache {
-    pub fn new<I>(cache_dir: I) -> Self
+    pub fn new<P>(cache_dir: P) -> anyhow::Result<Self>
     where
-        I: Into<PathBuf>
+        P: Into<PathBuf>
     {
-        Self {
-            dir: cache_dir.into()
+        let cache_dir = cache_dir.into();
+        if !cache_dir.try_exists()
+            .context("Could not verify that cache directory exists")? 
+        {
+            std::fs::create_dir_all(&cache_dir)?;
         }
+
+        Ok(Self {
+            cache_dir
+        })
     }
 
-    pub fn load<I>(&self, engine: &Engine, file_name: I) -> anyhow::Result<Module>
-    where
-        I: for<'a> Into<&'a Path>
-    {
-        let path = self.dir.join(file_name.into());
-        todo!();
+    pub fn load(&self, engine: &Engine, file_name: &str) -> anyhow::Result<Module> {    
+        let assembly_path = Path::new(ASSEMBLY_DIRECTORY).join(file_name);
+        let mut bytecode = Vec::new();
+        File::open(&assembly_path)
+            .context(format!("Could not find extension assembly {}", assembly_path.display()))?
+            .read_to_end(&mut bytecode)?;
+
+        let mut hasher = Sha256::new();
+        hasher.update(&bytecode);
+        let hash = hasher.finalize();
+        let hash_string = format!("{hash:x}");
+
+        let cache_path = self.cache_dir.join(hash_string);
+        if cache_path.try_exists()? {
+            tracing::info!("Loading cached '{file_name}' module");
+
+            // Load cache
+            Ok(unsafe {
+                Module::deserialize_file(engine, cache_path)?
+            })
+        } else {
+            tracing::info!("Precompiling extension module '{file_name}'");
+
+            let module = Module::new(engine, bytecode)?;
+            let serialized = module.serialize()?;
+
+            File::create(cache_path)?.write_all(&serialized)?;
+
+            Ok(module)
+        }
     }
 }
