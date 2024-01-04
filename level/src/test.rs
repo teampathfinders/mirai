3a63953ce3c4ce1a1bc2db2be67669c52679aed5
= <44266876+RadiatedMonkey@users.noreply.github.com>
2024-01-04 04:36:17 +0100

    Fix incorrect path in level test

--- level/src/test.rs
+++ level/src/test.rs
@@ -19,7 +19,7 @@ static LOCK: Mutex<()> = Mutex::new(());
 #[test]
 fn level_settings() {
     let _lock = LOCK.lock().unwrap();
-    let provider = unsafe { Provider::open("../../test-level").unwrap() };
+    let provider = unsafe { Provider::open("../test-level").unwrap() };
     let settings = provider.get_settings().unwrap();
 }
 //
