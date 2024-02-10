009de9a6cbbe95277d5adc09a8b847406328cb9b
Ruben Adema <44266876+RadiatedMonkey@users.noreply.github.com>
2024-02-10 19:10:26 +0100

    chore: document SetHud packet

--- protocol/src/bedrock/set_hud.rs
+++ protocol/src/bedrock/set_hud.rs
@@ -2,32 +2,50 @@ use util::{BinaryWrite, Serialize};
 
 use super::ConnectedPacket;
 
+/// The element to change in the HUD.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[repr(u8)]
 pub enum HudElement {
+    /// The small doll displayed in the top left corner when doing things such as sneaking.
     PaperDoll,
+    /// The armour points display.
     Armor,
+    /// Text shown above your hotbar when you switch to a different item.
     Tooltips,
+    /// Touch controls on touch devices.
     TouchControls,
+    /// The crosshair on screen.
     Crosshair,
+    /// The hotbar.
     Hotbar,
+    /// Health (only shown in survival)
     Health,
+    /// Experience progress bar.
     ProgressBar,
+    /// The hunger bar.
     Hunger,
+    /// Air bubbles in case the player is underwater.
     AirBubbles,
+    /// Health of the horse that is being ridden.
     HorseHealth
 }
 
+/// Visibility of a HUD element.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[repr(u8)]
 pub enum HudVisibility {
+    /// Hides the given HUD elements.
     Hide,
+    /// Makes the given HUD elements visible again.
     Reset
 }
 
+/// Hides or shows HUD elements.
 #[derive(Debug)]
 pub struct SetHud<'a> {
+    /// Elements to change.
     pub elements: &'a [HudElement],
+    /// New visibility of the given elements.
     pub visibibility: HudVisibility
 }
 
